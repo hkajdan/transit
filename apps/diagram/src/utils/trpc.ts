1db8a77d0e8eaebbec29904ebe0e4dd2fa2e06d0
Hugo Kajdan <hugo@kajdan.fr>
2026-02-24 10:57:30 +0100

    fixed api core package exports

--- apps/diagram/src/utils/trpc.ts
+++ apps/diagram/src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from "@repo/api-core";
+import type { AppRouter } from "@repo/api-core/router";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 
 export const { TRPCProvider, useTRPC, useTRPCClient } =
